6a9d2f0f5eb9a349a864911f540479174caac3df
Meagan Moore <meagan.moore@uberflip.com>
2020-08-28 14:22:43 -0400

    update accent color for blog post

--- src/components/Home/ShowcasedPosts/ShowcasedPostsContainer/ShowcasedPostCard/ShowcasedPostCard.tsx
+++ src/components/Home/ShowcasedPosts/ShowcasedPostsContainer/ShowcasedPostCard/ShowcasedPostCard.tsx
@@ -67,7 +67,7 @@ const Root = styled.section`
 
 const Title = styled.h4`
   margin: ${rhythm(0.4)} 0 ${rhythm(0.4)};
-  color: ${({ theme }) => theme.colors.accentPeach};
+  color: ${({ theme }) => theme.colors.accentBlue};
 
   @media ${device.tablet} {
     height: ${rhythm(2)};
